20b30a3ac615a1491f1f3d2e2f9b3d5907f60772
dclimber <dastand.climber@gmail.com>
2020-04-03 19:54:03 +0600

    feat: Add django-widget-tweaks

--- mapatest/settings/base.py
+++ mapatest/settings/base.py
@@ -33,19 +33,27 @@ SECRET_KEY = env('SECRET_KEY')
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django.contrib.sites',
+]
+
+AUTH_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 ]
+
+PROJECT_APPS = [
+    'widget_tweaks',
+]
+
+INSTALLED_APPS = DJANGO_APPS + AUTH_APPS + PROJECT_APPS
 
 SITE_ID = 1
 
