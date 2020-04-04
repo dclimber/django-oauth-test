54c73272139480ffb4ab6125e4bbdb263acdc766
dclimber <dastand.climber@gmail.com>
2020-04-04 21:52:49 +0600

    feat: Add modal form to show logged in users...
    ... that have newsletter_offered flag == False,
    a modal form asking them to subscribe to newsletter.

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
+
+    def post(self, request, *args, **kwargs):
+        choice = request.POST.get('choice')
+        profile = request.user.profile
+        if choice == 'Yes':
+            profile.in_newsletter = True
+        profile.newsletter_offered = True
+        profile.save()
+        return self.get(request, *args, **kwargs)
