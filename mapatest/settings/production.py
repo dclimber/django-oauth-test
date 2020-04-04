5c191dcf7e7b56f8828697f5cdc0a480d41d58e6
dclimber <dastand.climber@gmail.com>
2020-04-04 13:44:52 +0600

    feat: Add production.py settings file

--- mapatest/settings/production.py
+++ mapatest/settings/production.py
@@ -2,6 +2,22 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = [env('ALLOWED_HOST')]
+
+DATABASES = {
+    'default': env.db(),
+}
+
+# Email
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
 try:
     from .local import *
 except ImportError:
